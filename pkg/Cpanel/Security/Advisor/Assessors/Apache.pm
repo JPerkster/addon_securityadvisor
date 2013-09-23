454801588665c9554e5112374ecca5c5e7b5549f
Jacob Perkins <jacob.perkins@cpanel.net>
2013-09-23 13:14:12 -0500

    Not sure why this was reverted, resubmitting

--- pkg/Cpanel/Security/Advisor/Assessors/Apache.pm
+++ pkg/Cpanel/Security/Advisor/Assessors/Apache.pm
@@ -6,19 +6,19 @@ package Cpanel::Security::Advisor::Assessors::Apache;
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the owner nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# * Neither the name of the owner nor the names of its contributors may
+# be used to endorse or promote products derived from this software
+# without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL  BE LIABLE FOR ANY
+# DISCLAIMED. IN NO EVENT SHALL BE LIABLE FOR ANY
 # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -28,12 +28,13 @@ package Cpanel::Security::Advisor::Assessors::Apache;
 
 use strict;
 use base 'Cpanel::Security::Advisor::Assessors';
-use Cpanel::Version            ();
-use Cpanel::Config::Sources    ();
-use Cpanel::HttpRequest        ();
+use Cpanel::Version ();
+use Cpanel::Config::Sources ();
+use Cpanel::HttpRequest ();
 use Cpanel::HttpUtils::Version ();
-use Cpanel::SafeRun::Errors    ();
-
+use Cpanel::SafeRun::Errors ();
+use Cpanel::LoadFile ();
+use Cpanel::Logger ();
 sub version {
     return '1.03';
 }
@@ -78,13 +79,13 @@ sub _check_for_apache_chroot {
 
         $security_advisor_obj->add_advice(
             {
-                'type'       => $Cpanel::Security::Advisor::ADVISE_BAD,
-                'text'       => ['Apache vhosts are not segmented or chroot()ed.'],
+                'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
+                'text' => ['Apache vhosts are not segmented or chroot()ed.'],
                 'suggestion' => [
                     (
                         $Cpanel::Version::MAJORVERSION > 11.37
-                        ? 'Enable “Jail Apache” in the “[output,url,_1,Tweak Settings,_4,_5]” area, and change users to jailshell in the “[output,url,_2,Manage Shell Access,_4,_5]” area.  Consider a more robust solution by using “[output,url,_3,CageFS on CloudLinux,_4,_5]”'
-                        : 'Upgrade to cPanel 11.38 or later, then enable “Jail Apache” in the “[output,url,_1,Tweak Settings,_4,_5]” area, and change users to jailshell in the “[output,url,_2,Manage Shell Access,_4,_5]” area.  Consider a more robust solution by using “[output,url,_3,CageFS on CloudLinux,_4,_5]”'
+                        ? 'Enable “Jail Apache” in the “[output,url,_1,Tweak Settings,_4,_5]” area, and change users to jailshell in the “[output,url,_2,Manage Shell Access,_4,_5]” area. Consider a more robust solution by using “[output,url,_3,CageFS on CloudLinux,_4,_5]”'
+                        : 'Upgrade to cPanel 11.38 or later, then enable “Jail Apache” in the “[output,url,_1,Tweak Settings,_4,_5]” area, and change users to jailshell in the “[output,url,_2,Manage Shell Access,_4,_5]” area. Consider a more robust solution by using “[output,url,_3,CageFS on CloudLinux,_4,_5]”'
 
                     ),
                     $self->base_path('scripts2/tweaksettings?find=jailapache'),
@@ -101,12 +102,12 @@ sub _check_for_apache_chroot {
 }
 
 sub _check_for_easyapache_build {
-    my $self                 = shift;
+    my $self = shift;
     my $security_advisor_obj = $self->{'security_advisor_obj'};
 
-    my $cpsources          = Cpanel::Config::Sources::loadcpsources();
-    my $ea_update_server   = defined $cpsources->{'EASOURCES'} ? $cpsources->{'EASOURCES'} : $cpsources->{'HTTPUPDATE'};
-    my $httprequest_obj    = Cpanel::HttpRequest->new( 'hideOutput' => 1 );
+    my $cpsources = Cpanel::Config::Sources::loadcpsources();
+    my $ea_update_server = defined $cpsources->{'EASOURCES'} ? $cpsources->{'EASOURCES'} : $cpsources->{'HTTPUPDATE'};
+    my $httprequest_obj = Cpanel::HttpRequest->new( 'hideOutput' => 1 );
     my $latest_ea3_version = '';
     eval { $latest_ea3_version = $httprequest_obj->request( 'host' => $ea_update_server, 'url' => '/cpanelsync/easy/version_easy', 'protocol' => 0, ); };
     chomp($latest_ea3_version);
@@ -117,8 +118,8 @@ sub _check_for_easyapache_build {
     if ( $latest_ea3_version && $installed_version && $latest_ea3_version ne $installed_version ) {
         $security_advisor_obj->add_advice(
             {
-                'type'       => $Cpanel::Security::Advisor::ADVISE_WARN,
-                'text'       => ['EasyApache3 has updates available.'],
+                'type' => $Cpanel::Security::Advisor::ADVISE_WARN,
+                'text' => ['EasyApache3 has updates available.'],
                 'suggestion' => [
                     '[output,url,_1,EasyApache3,_2,_3] needs to be run periodically to update Apache, PHP and other public server functionality to the latest versions. Updates to EasyApache3 often fix security vulnernabilities in this software.',
                     $self->base_path('cgi/easyapache.pl?action=_pre_cpanel_sync_screen'),
@@ -139,8 +140,8 @@ sub _check_for_eol_apache {
     if ( $apache_version =~ /^(1\.3|2\.0)/ ) {
         $security_advisor_obj->add_advice(
             {
-                'type'       => $Cpanel::Security::Advisor::ADVISE_BAD,
-                'text'       => ['Your Apache version is EOL (End of Life)'],
+                'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
+                'text' => ['Your Apache version is EOL (End of Life)'],
                 'suggestion' => [
                     "Apache v${apache_version} is End of Life (EOL), which means it is no longer updated, and security holes will not be patched. Run [output,url,_1,EasyApache,_2,_3] and choose a newer, supported version.",
                     $self->base_path('cgi/easyapache.pl?action=_pre_cpanel_sync_screen'),
@@ -155,22 +156,22 @@ sub _check_for_eol_apache {
 
 sub _check_for_cdorked {
     my ($self) = @_;
-    my $result = `grep open_tty /usr/local/apache/bin/httpd`;
+    my $result = Cpanel::LoadFile::loadfile('/usr/local/apache/bin/httpd') or die Cpanel::Logger::info("Could not open Apache binary:: $!\n");
 
     if ( $result =~ /open_tty/) {
         $self->add_bad_advice (
-            'text'          =>      ['Apache binary seems to be infected by Linux/Cdorked.A'],
-            'suggestion'    =>      [
+            'text' => ['Apache binary may be infected by Linux/Cdorked.A'],
+            'suggestion' => [
                 'Please read this blog post "[output,url,_1,Apache binary backdoors,_2,_3]"',
                 'http://blog.sucuri.net/2013/04/apache-binary-backdoors-on-cpanel-based-servers.html',
                 'target',
                 '_blank'
             ],
         );
-    } 
+    }
     else {
         $self->add_good_advice (
-            'text'      =>      ["Apache binary appears to be clear of Linux/Cdorked.A:\n$result"]
+            'text' => ["Apache binary appears to be clear of Linux/Cdorked.A"]
         );
     }
     return 1;
